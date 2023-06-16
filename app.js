393ae80f3291e9b2721b08409d5849a4bc7affe1
Pritam96 <pritammondal96@gmail.com>
2023-06-16 08:34:01 +0530

    Using Nodemon for Autorestarts

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const routes = require('./routes');
 const server = http.createServer(routes);
 
 const PORT = 4000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
