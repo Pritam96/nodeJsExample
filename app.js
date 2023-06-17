0c3c5f6d1caff1622661e9e44024e76a0caa3f4d
Pritam96 <pritammondal96@gmail.com>
2023-06-17 20:28:28 +0530

    Installing Express & Adding Middleware

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+// creating express app
+const app = express();
+
+// adding middleware 1
+app.use((req, res, next) => {
+  console.log('In a middleware');
+  next(); // pass on to the next middleware
+});
+
+// adding middleware 2
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express!</h1>');
+});
 
 const PORT = 4000;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// It create const server = http.createServer(app) and
+// calls server.listen(PORT) automatically
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
