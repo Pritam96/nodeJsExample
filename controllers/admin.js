0229530d006d52a0771ca3ad2c20384d767186ad
Pritam96 <pritammondal96@gmail.com>
2023-06-28 14:37:22 +0530

    Inserting data & retrieving data, get single product, updating & deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,19 +13,22 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  // creating new product entry id=null
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => {
+      console.log('Product Added');
+      res.redirect('/admin/products');
     })
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([products]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('admin/products', {
         products: products,
         pageTitle: 'Admin Products',
@@ -41,15 +44,17 @@ exports.getEditProduct = (req, res, next) => {
     return redirect('/');
   }
   const productId = req.params.productId;
-  Product.findById(productId, (product) => {
-    if (!product) return redirect('/');
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      product: product,
-      editMode: editMode,
-    });
-  });
+  Product.findByPk(productId)
+    .then((product) => {
+      if (!product) return redirect('/');
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        product: product,
+        editMode: editMode,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -59,23 +64,29 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedDescription = req.body.description;
 
-  // updating an existing product entry, we send productId
-  // sequence: constructor(id, title, imageUrl, description, price)
-  const updatedProduct = new Product(
-    productId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(productId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDescription;
+      product.price = updatedPrice;
+      return product.save();
+    })
+    .then((result) => {
+      res.redirect('/admin/products');
+      console.log('Product Updated');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.deleteByID(productId)
-    .then(() => {
+  Product.findByPk(productId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log('Product Deleted');
       res.redirect('/admin/products');
     })
     .catch((err) => console.log(err));
