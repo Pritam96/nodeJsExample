91af60278bd1ddf33e95d6d002c9e4f8a06823dd
Pritam96 <pritammondal96@gmail.com>
2023-07-02 13:21:33 +0530

    Creating User model, Adding Product to User relationship, Creating Dummy user, fetching related products using magic association methods

--- app.js
+++ app.js
@@ -20,19 +20,47 @@ const errorController = require('./controllers/error');
 
 // Importing connection pool
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// creating a new middleware
+// manually setting req.user
+// req.user saved as a sequelize object
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// A Product is created by a User
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// A User can create multiple Product
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Pritam', email: 'pritam@example.com' });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(4000);
   })
   .catch((err) => console.log(err));
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then((result) => {
       console.log('Product Added');
       res.redirect('/admin/products');
@@ -27,7 +29,8 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render('admin/products', {
         products: products,
@@ -44,8 +47,10 @@ exports.getEditProduct = (req, res, next) => {
     return redirect('/');
   }
   const productId = req.params.productId;
-  Product.findByPk(productId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: productId } })
+    .then((products) => {
+      const product = products[0];
       if (!product) return redirect('/');
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
